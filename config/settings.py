c20f7ed744d19da05d6c3466556a760d6b9bcf22
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2025-06-07 22:38:52 +0300

    library 3

--- config/settings.py
+++ config/settings.py
@@ -130,16 +130,16 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = 'users.CustomUser'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST = os.getenv('EMAIL_HOST'),
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'sav4l@yandex.ru'
-EMAIL_HOST_PASSWORD = 'cpiwuabqzimiuibf'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER'),
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD'),
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-LOGIN_REDIRECT_URL = 'library:books_list'
-LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = os.getenv('LOGIN_REDIRECT_URL'),
+LOGIN_URL = os.getenv('LOGIN_URL'),
 
 CASHES = {
     'default': {
