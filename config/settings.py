cd3beb9bf9dc30d7554fb54810552ad9f99859ca
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2025-06-08 15:47:08 +0300

    CI7

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,9 @@ DATABASES = {
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("HOST"),
         "PORT": os.getenv("PORT"),
-
+        "POSTGRES_DB": os.getenv("POSTGRES_DB"),
+        "POSTGRES_USER": os.getenv("POSTGRES_USER"),
+        "POSTGRES_PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     }
 }
 
